1005834cc2b26402ba881f919a962eedb0a4793b
MariskaIJpelaar <m.m.j.ijpelaar@gmail.com>
2022-07-03 09:51:01 +0200

    adding local mvn install script

--- install_mvn.sh
+++ install_mvn.sh
@@ -13,13 +13,13 @@ scriptpath="$(dirname "$(realpath "$BASH_SOURCE")")"
 mkdir -p "$scriptpath/build"
 
 # Downloads maven 3.8.5
-maventar=$scriptpath/build/apache-maven-3.8.5-bin.tar.gz
+maventar=$scriptpath/build/apache-maven-3.8.6-bin.tar.gz
 if [[ ! -f "$maventar" ]]; then
   wget --quiet -P "$scriptpath/build" https://dlcdn.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.tar.gz
   tar --directory "$scriptpath"/build -xf "$maventar"
 fi
 
-mavenbin=$scriptpath/build/apache-maven-3.8.5/bin/mvn
+mavenbin=$scriptpath/build/apache-maven-3.8.6/bin/mvn
 if [[ ! -f "$mavenbin" ]]; then
   echo "Something went wrong with installing mvn"
   exit 1
