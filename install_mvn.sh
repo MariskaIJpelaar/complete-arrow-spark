b832625be9aa752c482c89580621a480c5b2fb61
MariskaIJpelaar <m.m.j.ijpelaar@gmail.com>
2022-07-03 09:43:14 +0200

    adding local mvn install script

---
+++ install_mvn.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# Installs mvn locally in the build directory
+if [[ -z $BASH_SOURCE ]]; then
+  echo "Expected BASH_SOURCE to be non-empty"
+  exit 1
+fi
+
+# Get path to script
+scriptpath="$(dirname "$(realpath "$BASH_SOURCE")")"
+
+# Creates the build-directory if it does not exist yet
+mkdir -p "$scriptpath/build"
+
+# Downloads maven 3.8.5
+maventar=$scriptpath/build/apache-maven-3.8.5-bin.tar.gz
+if [[ ! -f "$maventar" ]]; then
+  wget --quiet -P "$scriptpath/build" https://dlcdn.apache.org/maven/maven-3/3.8.5/binaries/apache-maven-3.8.5-bin.tar.gz
+  tar --directory "$scriptpath"/build -xf "$maventar"
+fi
+
+mavenbin=$scriptpath/build/apache-maven-3.8.5/bin/mvn
+if [[ ! -f "$mavenbin" ]]; then
+  echo "Something went wrong with installing mvn"
+  exit 1
+fi
+
+mavensym=$scriptpath/build/mvn
+if [[ ! -f "$mavensym" ]]; then
+  ln -s "$mavenbin" "$mavensym"
+fi
+
+$mavensym -v
+exitcode=$?
+test $exitcode -eq 0 || echo "Something went wrong with creating the symbolic link"
+exit $exitcode
+
+
